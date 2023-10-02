4386f34e2c7a59c187a37ad8e06210dc6b6058b1
Kana Sabaratnam <kana@cloudbasha.com>
2023-10-02 02:33:56 +0000

    CodeBridge commit

---
+++ react-frontend/src/components/ItemPage/SingleItemPage.js
@@ -0,0 +1,63 @@
+import { Button } from "primereact/button";
+import React, { useEffect, useState } from "react";
+import { connect } from "react-redux";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import client from "../../services/restClient";
+import { InputText } from 'primereact/inputtext';
+
+const SingleItemPage = (props) => {
+    const navigate = useNavigate();
+    const urlParams = useParams();
+    const [_entity, set_entity] = useState();
+    
+    useEffect(() => {
+        //on mount
+        client
+            .service("item")
+            .get(urlParams.singleItemId, { query: { $populate: [] }})
+            .then((res) => {
+                set_entity(res || {});
+                
+            })
+            .catch((error) => {
+                console.log({ error });
+                props.alert({ title: "Item", type: "error", message: error.message || "Failed get item" });
+            });
+    }, []);
+
+    const setValByKey = (key, val) => {
+        let new_entity = { ..._entity, [key]: val };
+        set_entity(new_entity);
+    };
+
+    const goBack = () => {
+        navigate("/item", { replace: true });
+    };
+    return (
+        <div className="col-12 flex flex-column align-items-center">
+            <div className="col-10">
+                <div className="flex align-items-center justify-content-start">
+                    <Button className="p-button-text" icon="pi pi-chevron-left" onClick={() => goBack()} />
+                    <h3 className="m-0">Item</h3>
+                </div>
+                <p>item/{urlParams.singleItemId}</p>
+            </div>
+            <div className="grid col-10">
+                <div className="card w-full">
+            
+            
+                </div>
+            </div>
+        </div>
+    );
+};
+
+const mapState = (state) => {
+    return {};
+};
+const mapDispatch = (dispatch) => ({
+    alert: (data) => dispatch.toast.alert(data),
+    //
+});
+
+export default connect(mapState, mapDispatch)(SingleItemPage);
